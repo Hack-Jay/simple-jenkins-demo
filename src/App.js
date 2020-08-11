20608fd54f50fee029b50dad1807dba425c6db10
jcgcdgj <908625658@qq.com>
2020-08-11 18:36:18 +0800

    feat: redux

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React from "react";
-import logo from "./logo.svg";
-// comp
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import MyRedux from "./MyRedux";
 
 import "./App.css";
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				{/* <p> */}
-					<MyRedux />
-				{/* </p> */}
-			</header>
-		</div>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Router>
+          <Link to="/" className="App-link">
+            Home
+          </Link>
+          <Link to="/redux" className="App-link">
+            MyRedux
+          </Link>
+          <br />
+          <Switch>
+            <Route component={MyRedux} exact path="/redux" />
+          </Switch>
+        </Router>
+      </header>
+    </div>
+  );
 }
 
 export default App;
